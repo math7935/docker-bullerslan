2e3ce8e16db4e7b67b08c6e0f06940dfa17e607d
Mathias Moldrup Larsen <44733475+math7935@users.noreply.github.com>
2023-11-12 21:07:39 +0100

    Fixer kaninens lort

--- launch.sh
+++ launch.sh
@@ -12,10 +12,10 @@ else
 	exit 99
 fi
 
-if ! [[ -f 'Server-Files-0.2.17.zip' ]]; then
+if ! [[ -f 'bullerslan-server-files-0.0.2.zip' ]]; then
 	rm -fr defaultconfigs config kubejs libraries mods Simple.zip forge*
 	curl -Lo 'bullerslan-server-files-0.0.2.zip' 'https://edge.forgecdn.net/files/4862/260/bullerslan-server-files-0.0.2.zip' || exit 9
-	unzip -u -o 'Server-Files-0.2.17.zip' -d /data
+	unzip -u -o 'bullerslan-server-files-0.0.2.zip' -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
 		cd "${DIR_TEST}"
